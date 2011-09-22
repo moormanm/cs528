ff4d10b8c7e61fba716c3fa886e5937db6e37b76
michael.e.moorman@gmail.com <michael.e.moorman@gmail.com@bc7d2a11-9811-4662-8751-9abf3bd84159>
2011-09-22 00:50:30 +0000

    updating

--- MissionariesAndCannibals/src/MissionariesAndCannibals.java
+++ MissionariesAndCannibals/src/MissionariesAndCannibals.java
@@ -6,7 +6,9 @@ public class MissionariesAndCannibals {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		BestFirstSearch bfs = new BestFirstSearch();
+		bfs.exec(3,3,1);
+		
 	}
 
 }
