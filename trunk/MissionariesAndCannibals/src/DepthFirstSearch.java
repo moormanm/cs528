91a868c5d1b0f338ae61c8ce3f4ba384e8bef849
kennylmay@gmail.com <kennylmay@gmail.com@bc7d2a11-9811-4662-8751-9abf3bd84159>
2011-09-22 01:50:02 +0000

    Updating

--- trunk/MissionariesAndCannibals/src/DepthFirstSearch.java
+++ trunk/MissionariesAndCannibals/src/DepthFirstSearch.java
@@ -1,162 +1,208 @@
 import java.util.*;
 
-
 /**
  * @author kenny
- *
+ * 
  */
 public class DepthFirstSearch {
-  
-	
-  class State{
-	  int rightCannibals;
-	  int rightMissionaries;
-	  int leftCannibals;
-	  int leftMissionaries;
-	  String boat;
 
-	 State(int lcann, int lmiss,int rcann, int rmiss, String bt){
-		  rightCannibals = rcann;
-		  leftCannibals = lcann;
-		  rightMissionaries = rmiss;
-		  leftMissionaries = lmiss;
-		  boat = bt;
-	 }
-	  
-	 State moveOneMissionary(){
-		State state;
-		if (this.boat == "R"){
-			state = new State(this.leftCannibals, this.leftMissionaries + 1, this.rightCannibals, this.rightMissionaries - 1,"L");
-		}else{
-			state = new State(this.leftCannibals, this.leftMissionaries - 1, this.rightCannibals, this.rightMissionaries + 1,"R");
+	class State {
+		int rightCannibals;
+		int rightMissionaries;
+		int leftCannibals;
+		int leftMissionaries;
+		String boat;
+
+		State(int lcann, int lmiss, int rcann, int rmiss, String bt) {
+			rightCannibals = rcann;
+			leftCannibals = lcann;
+			rightMissionaries = rmiss;
+			leftMissionaries = lmiss;
+			boat = bt;
 		}
-		return state;
-	 }
-	 
-	 State moveTwoMissionaries(){
-		 State state;
-		 if (this.boat == "R"){
-			 state = new State(this.leftCannibals, this.leftMissionaries + 2, this.rightCannibals, this.rightMissionaries - 2,"L");
-		 }else{
-			 state = new State(this.leftCannibals, this.leftMissionaries - 2, this.rightCannibals, this.rightMissionaries + 2,"R");
-		 }
-		 return state;
-	 }
-	 
-	 State moveOneCannabal(){
-		 State state;
-		 if (this.boat == "R"){
-			 state = new State(this.leftCannibals + 1, this.leftMissionaries, this.rightCannibals - 1, this.rightMissionaries,"L");
-		 }else{
-			 state = new State(this.leftCannibals - 1, this.leftMissionaries, this.rightCannibals + 1, this.rightMissionaries,"R");
-		 }
-		 return state;
-	 }
-	 
-	 State moveTwoCannabals(){
-		 State state;
-		 if (this.boat == "R"){
-			 state = new State(this.leftCannibals + 2, this.leftMissionaries, this.rightCannibals - 2, this.rightMissionaries,"L");
-		 }else{
-			 state = new State(this.leftCannibals - 2, this.leftMissionaries, this.rightCannibals + 2, this.rightMissionaries,"R");
-		 }
-		 return state;
-	 }
-	 
-	 State moveOneCannabelOneMissionary(){
-		 State state;
-		 if (this.boat == "R"){
-			 state = new State(this.leftCannibals + 1, this.leftMissionaries + 1, this.rightCannibals - 1, this.rightMissionaries - 1,"L");
-		 }else{
-			 state = new State(this.leftCannibals - 1, this.leftMissionaries - 1, this.rightCannibals + 1, this.rightMissionaries + 1,"R");
-		 }
-		 return state;
-	 }
-	  
-	 
-		 /*
+
+		State moveOneMissionary() {
+			State state;
+			if (this.boat == "R") {
+				state = new State(this.leftCannibals,
+						this.leftMissionaries + 1, this.rightCannibals,
+						this.rightMissionaries - 1, "L");
+			} else {
+				state = new State(this.leftCannibals,
+						this.leftMissionaries - 1, this.rightCannibals,
+						this.rightMissionaries + 1, "R");
+			}
+			return state;
+		}
+
+		State moveTwoMissionaries() {
+			State state;
+			if (this.boat == "R") {
+				state = new State(this.leftCannibals,
+						this.leftMissionaries + 2, this.rightCannibals,
+						this.rightMissionaries - 2, "L");
+			} else {
+				state = new State(this.leftCannibals,
+						this.leftMissionaries - 2, this.rightCannibals,
+						this.rightMissionaries + 2, "R");
+			}
+			return state;
+		}
+
+		State moveOneCannabal() {
+			State state;
+			if (this.boat == "R") {
+				state = new State(this.leftCannibals + 1,
+						this.leftMissionaries, this.rightCannibals - 1,
+						this.rightMissionaries, "L");
+			} else {
+				state = new State(this.leftCannibals - 1,
+						this.leftMissionaries, this.rightCannibals + 1,
+						this.rightMissionaries, "R");
+			}
+			return state;
+		}
+
+		State moveTwoCannabals() {
+			State state;
+			if (this.boat == "R") {
+				state = new State(this.leftCannibals + 2,
+						this.leftMissionaries, this.rightCannibals - 2,
+						this.rightMissionaries, "L");
+			} else {
+				state = new State(this.leftCannibals - 2,
+						this.leftMissionaries, this.rightCannibals + 2,
+						this.rightMissionaries, "R");
+			}
+			return state;
+		}
+
+		State moveOneCannabelOneMissionary() {
+			State state;
+			if (this.boat == "R") {
+				state = new State(this.leftCannibals + 1,
+						this.leftMissionaries + 1, this.rightCannibals - 1,
+						this.rightMissionaries - 1, "L");
+			} else {
+				state = new State(this.leftCannibals - 1,
+						this.leftMissionaries - 1, this.rightCannibals + 1,
+						this.rightMissionaries + 1, "R");
+			}
+			return state;
+		}
+
+		/*
 	  * 
 	  */
-	  boolean isValid(){
-		  // If we have no violated any rules then we can push a new state onto the queue
-		  if ((this.rightCannibals <= this.rightMissionaries) && (this.leftCannibals <= this.leftMissionaries) &&
-			  (this.rightCannibals >= 0) && (this.rightMissionaries >= 0) && 
-			  (this.leftCannibals >= 0) && (this.leftMissionaries >= 0)){
-			  return true;
-		  }     	
-		  
-		  return false;
-	  }  
-	  /*
-	   * Function used to see if the solution has been found.
-	   */
-      boolean isSolution(){
-    	  if(this.leftCannibals == 3 && this.leftMissionaries == 3 && this.boat == "R"){
-    		  return true;
-    	  }else{
-    		  return false;
-    	  }
-      }
-      
-      boolean newNode(Stack<State> stack){
+		boolean isValid() {
+			// If we have no violated any rules then we can push a new state
+			// onto the queue
+			if ((this.rightCannibals <= this.rightMissionaries)
+					&& (this.leftCannibals <= this.leftMissionaries)
+					&& (this.rightCannibals >= 0)
+					&& (this.rightMissionaries >= 0)
+					&& (this.leftCannibals >= 0)
+					&& (this.leftMissionaries >= 0)) {
+				return true;
+			}
 
-    	  stack.add(moveOneMissionary());
-          if (stack.peek().isSolution()){
-             return true;
-          }
-          if (stack.peek().isValid()) {
-        	  if (newNode(stack)) {
-        		  return true;
-        	  }
-          }
-          
-    	  stack.pop();
+			return false;
+		}
 
-    	  while(stack.peek().isValid()){
-             stack.add(moveTwoMissionaries()); 
-    	     if (stack.peek().isSolution()){
-         	    return true;
-             }
-    	  }
-    	  stack.pop();
-    	  
-    	  while(stack.peek().isValid()){
-    		 stack.add(moveOneCannabal()); 
-     	     if (stack.peek().isSolution()){
-          	    return true;
-             }
-     	  }
-     	  stack.pop();
-     	  
-     	 while(stack.peek().isValid()){
-     		 stack.add(moveTwoCannabals()); 
-    	     if (stack.peek().isSolution()){
-         	    return true;
-             }
-    	  }
-    	  stack.pop();
-    	  
-    	  while(stack.peek().isValid()){
-    		 stack.add(moveOneCannabelOneMissionary()); 
-     	     if (stack.peek().isSolution()){
-          	    return true;
-             }
-     	  }
-     	  stack.pop();
-     	  
-    	  return false;   	  
-      }
-  }
-  
+		/*
+		 * Function used to see if the solution has been found.
+		 */
+		boolean isSolution() {
+			if (this.leftCannibals == 3 && this.leftMissionaries == 3
+					&& this.boat == "R") {
+				return true;
+			} else {
+				return false;
+			}
+		}
 
-   DepthFirstSearch(){
+		State newNode(Stack<State> stack) {
+			State state;
 
-       Stack<State> stack = new Stack<State>();
-	   State state = new State(3, 3, 0, 0, "L");
-	   stack.add(state);
-	   state.newNode(stack);
-	   
-	   
-	   
-   }
+			state = moveOneMissionary();
+			if (state.isValid() && state.isUnique(state, stack)) {
+				return state;
+			}
+
+			state = moveTwoMissionaries();
+			if (state.isValid() && state.isUnique(state, stack)) {
+				return state;
+			}
+
+			state = moveOneCannabal();
+			if (state.isValid() && state.isUnique(state, stack)) {
+				return state;
+			}
+
+			state = moveTwoCannabals();
+			if (state.isValid() && state.isUnique(state, stack)) {
+				return state;
+			}
+
+			state = moveOneCannabelOneMissionary();
+			if (state.isValid() && state.isUnique(state, stack)) {
+				return state;
+			}
+			return null;
+		}
+
+		private boolean isUnique(State state, Stack<State> stack) {
+			int x = 0;
+			while (!isEqual(state, stack.elementAt(x))) {
+				if (isEqual(state, stack.elementAt(x))) {
+					return false;
+				}
+				x++;
+			}
+			return true;
+		}
+
+		private boolean isEqual(State state1, State state2) {
+			if ((state1.leftCannibals == state2.leftCannibals)
+					&& (state1.leftMissionaries == state2.leftMissionaries)
+					&& (state1.rightCannibals == state2.rightCannibals)
+					&& (state1.rightMissionaries == state2.rightMissionaries)
+					&& (state1.boat == state2.boat)) {
+				return true;
+			}
+			return false;
+
+		}
+
+		public String toString(State state) {
+			String text = state.leftCannibals + " " + state.leftMissionaries
+					+ " " + state.boat;
+			return text;
+
+		}
+	}
+
+	DepthFirstSearch() {
+
+		Stack<State> stack = new Stack<State>();
+		State state = new State(3, 3, 0, 0, "L");
+		stack.add(state);
+
+		while (stack.peek().isValid()) {
+			System.out.println(state.toString(stack.peek()));
+			if (stack.peek().isSolution()) {
+				System.out.println("Found Solution");
+			} else {
+				if (state.newNode(stack) != null) {
+					stack.add(state);
+				}
+			}
+			if (stack.isEmpty()) {
+				System.out.println("No Solution Found");
+			}
+
+			state = stack.firstElement();
+			stack.remove(0);
+		}
+	}
 }
