495bb9854ef6e73317d36a5ba89849ba9c5036a9
kennylmay@gmail.com <kennylmay@gmail.com@bc7d2a11-9811-4662-8751-9abf3bd84159>
2011-10-28 02:37:19 +0000

    Adding tree builder function

--- LeagueOfLegendsHelper/src/LeagueOfLegendsHelper.java
+++ LeagueOfLegendsHelper/src/LeagueOfLegendsHelper.java
@@ -8,6 +8,7 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map.Entry;
+import java.util.Vector;
 
 
 
@@ -15,7 +16,7 @@ public class LeagueOfLegendsHelper {
 
 
 	public static HashMap<String, LoLItem> items = getFactBase();
-	
+	public static HashMap<String, String[]> itemTree = getItemTree();
 	
 	//Parse the CSV file into HashMap of items. These are the facts used by the expert system.
 	public static HashMap<String, LoLItem>  getFactBase() {
@@ -37,7 +38,6 @@ public class LeagueOfLegendsHelper {
 			//		System.out.print(fieldNames[i] + " is " + fields[i] + ", ");
 					item.put(fieldNames[i], fields[i]);
 				}
-				System.out.println("");
 				
 				//The name of the item is field 0
 				ret.put((String)item.get(fieldNames[0]), item);
@@ -60,6 +60,50 @@ public class LeagueOfLegendsHelper {
 		
 	}
 	
+	//Parse the CSV file into HashMap of items. This will be used to reconstruct item trees.
+		public static HashMap<String, String[]>  getItemTree() {
+			String csvPath = LeagueOfLegendsHelper.class.getResource("LoL_Item_Tree.csv").getPath();
+			
+			try {
+				BufferedReader in = new BufferedReader(new FileReader(csvPath));
+				HashMap<String, String[]> ret = new HashMap<String, String[]>();
+			
+				// Read Past the header line
+			    in.readLine();
+				
+				String line;
+				String parentName;
+				// If the parent is the only item just add the parent. If the parent has children 
+				// in the line then split the line by commas and add both to the hashmap
+			    while( (line = in.readLine()) != null) {
+			    	if (line.indexOf(',') == -1){
+			    		parentName = line;	
+			    		ret.put(parentName, null);
+			    	}else{
+			    		parentName = line.substring(0,line.indexOf(',', 0));
+			    		line = line.substring(line.indexOf(',', 0)+1);
+			    		String[] childNames = line.split(",");									
+			    
+			    		ret.put(parentName, childNames);
+			    	}
+				}
+				return ret;
+				
+				
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				System.exit(1);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				System.exit(1);
+			}
+			
+			//Never gets here
+			return null;
+			
+		}
 	/**
 	 * 
 	 * @param Item Name of the item to check for attribute.
@@ -71,6 +115,25 @@ public class LeagueOfLegendsHelper {
 	  // Return true if the item has the attribute and the attribute is not empty.
 	  return !items.get(Item).get(Attribute).toString().isEmpty();
 
+	}
+	
+	
+	static Vector<String> getAllChildren(String Item){
+		Vector<String> children = new Vector<String>();
+		if(itemTree.get(Item) == null){
+			return null;
+		}
+		
+		String[] Items = itemTree.get(Item);
+		for(String item : Items){
+			children.add(item);
+			String[] subItems = itemTree.get(item);
+			if (subItems != null)
+			for(String subItem : subItems){
+				children.add(subItem);
+			}		
+		}
+		return children;
 	}
 	
 	/**
@@ -95,7 +158,6 @@ public class LeagueOfLegendsHelper {
 		}
 		
 		return ret;
-		
 	}
 	
 	
@@ -108,6 +170,7 @@ public class LeagueOfLegendsHelper {
 		System.out.println(hasAttribute("Phantom Dancer", "Damage"));
 		
 		System.out.println(getAttributeItems("Damage").toString());
+		System.out.println(getAllChildren("Phantom Dancer").toString());
 	}
 
 }
