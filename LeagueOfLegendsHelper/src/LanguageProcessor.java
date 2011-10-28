3c392b67e057499a2ab6cdadc4ac4503ad7e3085
kennylmay@gmail.com <kennylmay@gmail.com@bc7d2a11-9811-4662-8751-9abf3bd84159>
2011-10-28 16:18:36 +0000

    Adding basic Language Processing framework. This will need to be expanded later, but for now it gives us something to test with.

---
+++ LeagueOfLegendsHelper/src/LanguageProcessor.java
@@ -0,0 +1,122 @@
+import javax.swing.JOptionPane;
+import java.util.HashMap;
+import java.util.Vector;
+
+public class LanguageProcessor {
+	HashMap<String, Vector<String>> synonyms = new HashMap<String, Vector<String>>();
+	Vector<String> uselessWords = new Vector<String>();
+	
+	/**
+	 * Only setup the word lists in the constructor.
+	 */
+	public LanguageProcessor(){
+		intializeThesaurus();
+		intializeUselessWords();
+	}
+	
+	/**
+	 * Ask the user for an English sentence that we will then try to interpret
+	 * into its very basic meaning.
+	 * 
+	 * @return The user's "Meaning" of the sentence they typed
+	 */
+	public String askQuestion(){
+		String userInput = JOptionPane.showInputDialog("What can I help you with?");
+		if (userInput == null){
+			return "";
+		}
+		
+		String meaning = userInput.toLowerCase();
+	    meaning = filterUselessWords(meaning);
+		meaning = determineMeaning(meaning);
+		
+		return meaning;
+	}
+	
+	/**
+	 * This function will remove any words that are included in
+	 * the uselessWords Vector. This function will also remove
+	 * any unneeded punctuation.
+	 * 
+	 * @return The sentence without meaningless words.
+	 */
+	private String filterUselessWords(String sentence){
+		sentence = sentence.replaceAll("'","");
+		sentence = sentence.replaceAll(".",""); 
+		sentence = sentence.replaceAll(",",""); 
+		
+		for(String word : uselessWords){
+		  sentence = sentence.replaceAll(word + " ", "");	
+		  sentence = sentence.replaceAll(" " + word, "");	
+		}
+		return sentence;
+	}
+	
+	/**
+	 * This function will replace the user's words with words
+	 * that our program can understand. It will find any words
+	 * defined in the "synonyms" HashMap and replace them with 
+	 * their key word.
+	 * 
+	 * @return The sentence with only key words.
+	 */
+	private String determineMeaning(String sentence){
+		for(String word: synonyms.keySet()){
+			for (String syn : synonyms.get(word)){
+				sentence = sentence.replaceAll(syn, word);
+			}
+		}
+		return sentence;
+	}
+	
+	/**
+	 * Setup the synonym list. The baseWord is the word you would
+	 * like the word or phrase to be replaced with.
+	 */
+	private void intializeThesaurus(){
+		String baseWord;
+		Vector<String> syns = new Vector<String>();
+		
+		baseWord = "Damage";	
+		syns.add("attack damage");
+		syns.add("physical damage");
+		syns.add("need more damage");
+		syns.add("ad");
+		syns.add("kill people");
+		syns.add("kill");
+		this.synonyms.put(baseWord, syns);
+		
+		syns = new Vector<String>();
+		baseWord = "Tanky";
+		syns.add("unkillable");
+		syns.add("hard to kill");
+		syns.add("keep dieing");
+		syns.add("easily killed");
+		this.synonyms.put(baseWord, syns);
+		
+		syns = new Vector<String>();
+		baseWord = "Mana";
+		syns.add("mana");
+		syns.add("spam ablities");
+		syns.add("cant use ablities");
+		syns.add("need more mana");
+		this.synonyms.put(baseWord, syns);
+	}
+	
+	/**
+	 * Setup a list of words that do not have any real 
+	 * meaning in the sentence.
+	 */
+	private void intializeUselessWords(){
+		uselessWords.add("want");
+		uselessWords.add("i");
+		uselessWords.add("am");
+		uselessWords.add("being");
+		uselessWords.add("real");
+		uselessWords.add("my");
+		uselessWords.add("enough");
+		uselessWords.add("to");
+	}
+	
+}
+
--- LeagueOfLegendsHelper/src/LeagueOfLegendsHelper.java
+++ LeagueOfLegendsHelper/src/LeagueOfLegendsHelper.java
@@ -3,48 +3,50 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map.Entry;
+import java.util.Set;
 import java.util.Vector;
 
-
+import javax.swing.JOptionPane;
 
 public class LeagueOfLegendsHelper {
 
-
 	public static HashMap<String, LoLItem> items = getFactBase();
 	public static HashMap<String, String[]> itemTree = getItemTree();
-	
-	//Parse the CSV file into HashMap of items. These are the facts used by the expert system.
-	public static HashMap<String, LoLItem>  getFactBase() {
-		String csvPath = LeagueOfLegendsHelper.class.getResource("LoL_Items_CSV.csv").getPath();
-		
+
+	// Parse the CSV file into HashMap of items. These are the facts used by the
+	// expert system.
+	public static HashMap<String, LoLItem> getFactBase() {
+		String csvPath = LeagueOfLegendsHelper.class.getResource(
+				"LoL_Items_CSV.csv").getPath();
+
 		try {
 			BufferedReader in = new BufferedReader(new FileReader(csvPath));
 			HashMap<String, LoLItem> ret = new HashMap<String, LoLItem>();
-		
+
 			String headerLine = in.readLine();
 			String[] fieldNames = headerLine.split(",");
-			
-			
+
 			String line;
-			while( (line = in.readLine()) != null) {
+			while ((line = in.readLine()) != null) {
 				LoLItem item = new LoLItem();
 				String[] fields = line.split(",");
-				for(int i=0; i< fields.length; i ++) {
-			//		System.out.print(fieldNames[i] + " is " + fields[i] + ", ");
+				for (int i = 0; i < fields.length; i++) {
+					// System.out.print(fieldNames[i] + " is " + fields[i] +
+					// ", ");
 					item.put(fieldNames[i], fields[i]);
 				}
-				
-				//The name of the item is field 0
-				ret.put((String)item.get(fieldNames[0]), item);
+
+				// The name of the item is field 0
+				ret.put((String) item.get(fieldNames[0]), item);
 			}
 			return ret;
-			
-			
+
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -54,123 +56,126 @@ public class LeagueOfLegendsHelper {
 			e.printStackTrace();
 			System.exit(1);
 		}
-		
-		//Never gets here
+
+		// Never gets here
 		return null;
-		
+
 	}
-	
-	//Parse the CSV file into HashMap of items. This will be used to reconstruct item trees.
-		public static HashMap<String, String[]>  getItemTree() {
-			String csvPath = LeagueOfLegendsHelper.class.getResource("LoL_Item_Tree.csv").getPath();
-			
-			try {
-				BufferedReader in = new BufferedReader(new FileReader(csvPath));
-				HashMap<String, String[]> ret = new HashMap<String, String[]>();
-			
-				// Read Past the header line
-			    in.readLine();
-				
-				String line;
-				String parentName;
-				// If the parent is the only item just add the parent. If the parent has children 
-				// in the line then split the line by commas and add both to the hashmap
-			    while( (line = in.readLine()) != null) {
-			    	if (line.indexOf(',') == -1){
-			    		parentName = line;	
-			    		ret.put(parentName, null);
-			    	}else{
-			    		parentName = line.substring(0,line.indexOf(',', 0));
-			    		line = line.substring(line.indexOf(',', 0)+1);
-			    		String[] childNames = line.split(",");									
-			    
-			    		ret.put(parentName, childNames);
-			    	}
+
+	// Parse the CSV file into HashMap of items. This will be used to
+	// reconstruct item trees.
+	public static HashMap<String, String[]> getItemTree() {
+		String csvPath = LeagueOfLegendsHelper.class.getResource(
+				"LoL_Item_Tree.csv").getPath();
+
+		try {
+			BufferedReader in = new BufferedReader(new FileReader(csvPath));
+			HashMap<String, String[]> ret = new HashMap<String, String[]>();
+
+			// Read Past the header line
+			in.readLine();
+
+			String line;
+			String parentName;
+			// If the parent is the only item just add the parent. If the parent
+			// has children
+			// in the line then split the line by commas and add both to the
+			// hashmap
+			while ((line = in.readLine()) != null) {
+				if (line.indexOf(',') == -1) {
+					parentName = line;
+					ret.put(parentName, null);
+				} else {
+					parentName = line.substring(0, line.indexOf(',', 0));
+					line = line.substring(line.indexOf(',', 0) + 1);
+					String[] childNames = line.split(",");
+
+					ret.put(parentName, childNames);
 				}
-				return ret;
-				
-				
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				System.exit(1);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				System.exit(1);
 			}
-			
-			//Never gets here
-			return null;
-			
+			return ret;
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.exit(1);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.exit(1);
 		}
+
+		// Never gets here
+		return null;
+
+	}
+
 	/**
 	 * 
-	 * @param Item Name of the item to check for attribute.
-	 * @param Attribute Name of the attribute to check for.
-	 * @return True if the attribute value is not empty. 
+	 * @param Item
+	 *            Name of the item to check for attribute.
+	 * @param Attribute
+	 *            Name of the attribute to check for.
+	 * @return True if the attribute value is not empty.
 	 */
-	static boolean hasAttribute(String Item, String Attribute){
+	static boolean hasAttribute(String Item, String Attribute) {
 
-	  // Return true if the item has the attribute and the attribute is not empty.
-	  return !items.get(Item).get(Attribute).toString().isEmpty();
+		// Return true if the item has the attribute and the attribute is not
+		// empty.
+		return !items.get(Item).get(Attribute).toString().isEmpty();
 
 	}
-	
-	
-	static Vector<String> getAllChildren(String Item){
+
+	static Vector<String> getAllChildren(String Item) {
 		Vector<String> children = new Vector<String>();
-		if(itemTree.get(Item) == null){
+		if (itemTree.get(Item) == null) {
 			return null;
 		}
-		
+
 		String[] Items = itemTree.get(Item);
-		for(String item : Items){
+		for (String item : Items) {
 			children.add(item);
 			String[] subItems = itemTree.get(item);
 			if (subItems != null)
-			for(String subItem : subItems){
-				children.add(subItem);
-			}		
+				for (String subItem : subItems) {
+					children.add(subItem);
+				}
 		}
 		return children;
 	}
-	
+
 	/**
 	 * 
-	 * @param Attribute Attribute to search all items for.
+	 * @param Attribute
+	 *            Attribute to search all items for.
 	 * @return List of Item names that have the requested Attribute
 	 */
 	static public List<String> getAttributeItems(String Attribute) {
-		
-		Iterator<Entry <String, LoLItem>> iter = items.entrySet().iterator();
+
+		Iterator<Entry<String, LoLItem>> iter = items.entrySet().iterator();
 		LinkedList<String> ret = new LinkedList<String>();
-		
+
 		String currentKey;
-		
-		
-		while(iter.hasNext()) {
+
+		while (iter.hasNext()) {
 			currentKey = iter.next().getKey();
-			
+
 			if (hasAttribute(currentKey, Attribute)) {
 				ret.add(currentKey);
 			}
 		}
-		
+
 		return ret;
 	}
-	
-	
-	
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println(hasAttribute("Long Sword", "Damage"));
-		System.out.println(hasAttribute("Phantom Dancer", "Damage"));
-		
-		System.out.println(getAttributeItems("Damage").toString());
-		System.out.println(getAllChildren("Phantom Dancer").toString());
-	}
 
+		LanguageProcessor user = new LanguageProcessor();
+		String meaning = user.askQuestion();
+		System.out.println(meaning);
+		JOptionPane.showMessageDialog(null, getAttributeItems(meaning).toString());
+	}
 }
