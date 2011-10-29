569d674e980cb2a909628e155ade9528d3b4932d
michael.e.moorman@gmail.com <michael.e.moorman@gmail.com@bc7d2a11-9811-4662-8751-9abf3bd84159>
2011-10-29 17:00:02 +0000

    more stubs

--- trunk/LeagueOfLegendsHelper/src/Expert.java
+++ trunk/LeagueOfLegendsHelper/src/Expert.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 
 
@@ -6,13 +7,42 @@ public class Expert {
 	@SuppressWarnings("unchecked")
 	static Answer suggestNextItem(CaseData caseData, HashMap<String, LoLItem> itemData, HashMap<String, String[]> itemHierarchy) {
 		
-		//Translate the current facts into statistical data. This can be used for heuristics later.
+		//Translate the currently possessed items of both players into statistical data. This will be used for heuristics later.
 		PlayerStats playerStats = PlayerStats.calculateStats( (LinkedList<LoLItem>) caseData.get("playerItems"));
 		PlayerStats opponentStats = PlayerStats.calculateStats((LinkedList<LoLItem>) caseData.get("opponentItems"));
 		
-
-
+		//Get the core attributes for each player's role		
+		HashSet<String> playerCore =   coreAttributesFor( (ChampionRole) caseData.get("playerChampionRole"));
+		HashSet<String> opponentCore = coreAttributesFor( (ChampionRole) caseData.get("opponentChampionRole"));
 		
+		//Get the list of general counters for each of the opponents core attributes
+		LinkedList<Pair<String, String>> counters = new LinkedList<Pair<String, String>>();
+		for(String attr : opponentCore) {
+			String c = counterFor(attr);
+			if(c.equals("")) {
+				continue;
+			}
+			counters.add(new Pair<String, String>(attr,c));
+		}
+		
+		//If there are any general counters available, check to see if there is a disparity between
+		//the attribute and the counter. If there is, add it to the disparate properties list.
+		LinkedList<Pair< Pair<String, String>, Integer>>  disparateProperties = new LinkedList<Pair< Pair<String,String>, Integer>>();
+		for(Pair<String, String> p : counters) {
+			//Get the heuristic value of each 
+			int opponentValForAttr = heuristicForAttribute(p.first, opponentStats.get(p.first));
+			int playerValForCounter = heuristicForAttribute(p.second, playerStats.get(p.second));
+			
+			if(opponentValForAttr > playerValForCounter) {
+				//Add the pairing and the difference in heuristic value to the list of disparities
+				disparateProperties.add(new Pair< Pair<String,String>, Integer>(p, opponentValForAttr - playerValForCounter));
+			}
+		}
+		
+		//Apply exceptions here. Remove any disparity from the disparity list that we don't care about. 
+		  //For instance, if the opponent is a support character, don't care about the ability power/cdr/magic pen VS magic resist disparity.
+		  //Another example, if the opponent is a mage, don't care about the attack damage/attack speed/ crit chance VS armor disparity. 
+		  //Another example, if the player is support, don't care about the ability power VS magic pen disparity.
 		
 
 		
@@ -23,6 +53,9 @@ public class Expert {
 	}
  
 	
+
+
+
 	public class Answer {
 	   String response;
 	   LinkedList<String> items;
@@ -32,6 +65,66 @@ public class Expert {
 	public enum ChampionRole { Assasin, Tank, Mage, Fighter, Support };
 	
 	
+	//This function returns the core attributes for a type of champ; for example, a Fighter would have attack damage, armor, armor pen, health, etc
+	public static HashSet<String> coreAttributesFor(ChampionRole c) {
+		return null;
+	}
+	
+	
+    private static String counterFor(String attr) {
+    	if(attr.equals("Health")) {
+    		return "Damage";
+    	}
+    	else if(attr.equals("Armor")) {
+    		return "ArmorPenetration";
+    	}
+    	
+    	else if(attr.equals("ArmorPenetration")) {
+    		return "Armor";
+    	}
+    	else if(attr.equals("AbilityPower")) {
+    		return "MagicResist";
+    		
+    	}
+    	else if(attr.equals("MagicResist")) {
+    		return "MagicPenetration";
+    	}
+    	else if(attr.equals("MagicPenetration")) {
+    		return "MagicResist";
+    	}
+    	else if(attr.equals("Critical")) {
+    		return "Armor";
+    	}
+    	else if(attr.equals("AttackSpeed")) {
+    		return "Armor";
+    	}
+    	else if(attr.equals("LifeSteal")) {
+    		return "Armor";
+    	}
+    	else if(attr.equals("SpellVamp")) {
+    		return "MagicResist";
+    	}
+    	else if(attr.equals("CDR")) {
+    		return "MagicResist";
+    	}
+
+    	
+    	//There is no counter for this attribute.
+    	return "";
+    	
+    	
+    }
+    
+    //This function will assign a value to an investment in an attribute. It should return a number greater than or equal to 0. 
+    //For example, if the attr is "Attack damage" and the val is "144":
+       //Say the heuristic for attack damage is that it generally costs 1000 gold to get 30 damage This would equate
+       //to a total investment of 144 * (1000 / 30)  = 4800 gold
+       //Then assign a heuristic based on the gold investment value, maybe total estimated investment / 100
+       //This would make the total heuristic value = round(4800 / 100) = 5 
+	private static int heuristicForAttribute(String attr, Object val) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 	
 }
 
---
+++ trunk/LeagueOfLegendsHelper/src/Pair.java
@@ -0,0 +1,10 @@
+
+public class Pair<T,T2> {
+	public T first;
+	public T2 second;
+	public Pair(T a, T2 b) {
+		this.first = a; 
+		this.second = b;
+	}
+
+}
