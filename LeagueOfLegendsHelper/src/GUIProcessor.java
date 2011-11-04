4bf89e89e3a648f8c6dadd182b86bc6f2b275322
kennylmay@gmail.com <kennylmay@gmail.com@bc7d2a11-9811-4662-8751-9abf3bd84159>
2011-11-04 00:02:26 +0000

    Limiting the number of characteristics a champion can have

--- LeagueOfLegendsHelper/src/GUIProcessor.java
+++ LeagueOfLegendsHelper/src/GUIProcessor.java
@@ -11,9 +11,6 @@ public class GUIProcessor {
 			LinkedList<Token> playerCore = getAttributes(
 					(String[]) caseData.get("playerChampionRole"), true);
 			if (caseData.get("opponentChampionRole") != null) {
-				playerCore.add(new Token(Token.Typ.LogicalOp,
-						Token.LogicalOpTyp.AND));
-
 				LinkedList<Token> opponentCore = getAttributes(
 						(String[]) caseData.get("opponentChampionRole"), false);
 				playerCore.addAll(opponentCore);
@@ -36,49 +33,26 @@ public class GUIProcessor {
 			attr.add(new Token(Token.Typ.Player, Token.PlayerTyp.Us));
 
 		for (String champ : c)
-			if (champ == "assasin") {
-				attr.add(new Token(Token.Typ.Attribute, "ArmorPenetration"));
+			if (champ.equals("assasin")) {
 				attr.add(new Token(Token.Typ.Attribute, "Critical"));
-				attr.add(new Token(Token.Typ.Attribute, "Damage"));
 			} else if (champ.equals("tank")) {
-				attr.add(new Token(Token.Typ.Attribute, "Armor"));
 				attr.add(new Token(Token.Typ.Attribute, "Health"));
-				attr.add(new Token(Token.Typ.Attribute, "MagicResist"));
 			} else if (champ.equals("mage")) {
 				attr.add(new Token(Token.Typ.Attribute, "AbilityPower"));
-				attr.add(new Token(Token.Typ.Attribute, "MagicPenetration"));
-				attr.add(new Token(Token.Typ.Attribute, "CDR"));
-				attr.add(new Token(Token.Typ.Attribute, "SpellVamp"));
 			} else if (champ.equals("fighter")) {
-				attr.add(new Token(Token.Typ.Attribute, "AttackSpeed"));
-				attr.add(new Token(Token.Typ.Attribute, "Critical"));
 				attr.add(new Token(Token.Typ.Attribute, "Damage"));
-				attr.add(new Token(Token.Typ.Attribute, "LifeSteal"));
 			} else if (champ.equals("support")) {
-				attr.add(new Token(Token.Typ.Attribute, "AbilityPower"));
 				attr.add(new Token(Token.Typ.Attribute, "CDR"));
-				attr.add(new Token(Token.Typ.Attribute, "SpellVamp"));
 			} else if (champ.equals("melee")) {
-				attr.add(new Token(Token.Typ.Attribute, "Armor"));
-				attr.add(new Token(Token.Typ.Attribute, "AttackSpeed"));
-				attr.add(new Token(Token.Typ.Attribute, "Damage"));
 				attr.add(new Token(Token.Typ.Attribute, "LifeSteal"));
 			} else if (champ.equals("stealth")) {
 				attr.add(new Token(Token.Typ.Attribute, "AttackSpeed"));
-				attr.add(new Token(Token.Typ.Attribute, "Damage"));
 			} else if (champ.equals("carry")) {
-				attr.add(new Token(Token.Typ.Attribute, "AttackSpeed"));
 				attr.add(new Token(Token.Typ.Attribute, "Damage"));
-				attr.add(new Token(Token.Typ.Attribute, "Critical"));
-				attr.add(new Token(Token.Typ.Attribute, "LifeSteal"));
 			} else if (champ.equals("pusher")) {
 				attr.add(new Token(Token.Typ.Attribute, "Armor"));
-				attr.add(new Token(Token.Typ.Attribute, "Damage"));
-				attr.add(new Token(Token.Typ.Attribute, "Health"));
 			} else if (champ.equals("ranged")) {
 				attr.add(new Token(Token.Typ.Attribute, "AttackSpeed"));
-				attr.add(new Token(Token.Typ.Attribute, "Damage"));
-				attr.add(new Token(Token.Typ.Attribute, "LifeSteal"));
 			}
 		return attr;
 	}
