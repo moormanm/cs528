9aa817a9e680184fcb36fabb03af9c440bde5263
kresss <kresss@bc7d2a11-9811-4662-8751-9abf3bd84159>
2011-11-12 18:08:32 +0000

    Adding endless reading from stdin.

--- chatbot/chatbot/src/chatbot/chatbot.java
+++ chatbot/chatbot/src/chatbot/chatbot.java
@@ -1,7 +1,9 @@
 package chatbot;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import opennlp.tools.cmdline.parser.ParserTool;
 import opennlp.tools.dictionary.Dictionary;
@@ -23,45 +25,39 @@ public class chatbot {
 	public static TokenizerME tokenizer = null;
 
 	public static SentenceModel sm = null;
-	public static TokenizerModel tm = null;	
+	public static TokenizerModel tm = null;
 	public static Dictionary dict = null;
 	public static ParserModel pm = null;
 	public static Parser parser = null;
 	public static POSModel posModel = null;
 	public static POSTaggerME tagger = null;
-	
-	public static void loadModels() {
-	
 
-        
-		
+	private static BufferedReader stdin = null;
+
+	public static void loadModels() {
+
 		try {
-			//Sentence model
-			InputStream res = chatbot.class
-			.getResourceAsStream("/en-sent.bin");
-			
+			// Sentence model
+			InputStream res = chatbot.class.getResourceAsStream("/en-sent.bin");
+
 			sm = new SentenceModel(res);
 			sdetector = new SentenceDetectorME(sm);
-			
-			
-			//Tokenizer model
-			res = chatbot.class
-			.getResourceAsStream("/en-token.bin");
+
+			// Tokenizer model
+			res = chatbot.class.getResourceAsStream("/en-token.bin");
 			tm = new TokenizerModel(res);
 			tokenizer = new TokenizerME(tm);
-			
-			//POSModel
-			res = chatbot.class
-			.getResourceAsStream("/en-pos-maxent.bin");
+
+			// POSModel
+			res = chatbot.class.getResourceAsStream("/en-pos-maxent.bin");
 			posModel = new POSModel(res);
 			tagger = new POSTaggerME(posModel);
-			
-			//ParserModel
-			res = chatbot.class
-			.getResourceAsStream("/en-parser-chunking.bin");
+
+			// ParserModel
+			res = chatbot.class.getResourceAsStream("/en-parser-chunking.bin");
 			pm = new ParserModel(res);
 			parser = ParserFactory.create(pm);
-			
+
 		} catch (InvalidFormatException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -69,31 +65,44 @@ public class chatbot {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
 
 	}
-	
+
+	public static String readInput() {
+
+		System.out.print("\nWhat are you thinking???\n\n");
+
+		String retString = "";
+
+		try {
+			retString = stdin.readLine();
+		} catch (IOException ioe) {
+			System.out.println("IO error reading input!");
+			System.exit(1);
+		}
+
+		return retString;
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Initializing...");
 		loadModels();
+		// open up standard input
+		stdin = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Done");
-		
-		//Parse a sentence
-		String sentences[] = sdetector.sentDetect("The quick brown fox jumped over the lazy dog.");
-		for(String sent : sentences) {
-			Parse[] topParses = ParserTool.parseLine(sent, parser, 1);
-			
-			for(Parse p : topParses) {
-			  p.show();
+
+		while (true) {
+			// Parse a sentence
+			String sentences[] = sdetector.sentDetect(readInput());
+			for (String sent : sentences) {
+				Parse[] topParses = ParserTool.parseLine(sent, parser, 1);
+
+				for (Parse p : topParses) {
+					p.show();
+				}
 			}
 		}
-		
 
-		
 	}
-	
-}
-	
-		
 
+}
