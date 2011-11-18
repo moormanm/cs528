91f2f2f585b490ad4826f1235f23f3208de0d896
kennylmay@gmail.com <kennylmay@gmail.com@bc7d2a11-9811-4662-8751-9abf3bd84159>
2011-11-18 01:11:20 +0000

    Reading from a file and adding a dictionary to hold examples of the kinds of parts of speech.

--- chatbot/chatbot/src/chatbot/chatbot.java
+++ chatbot/chatbot/src/chatbot/chatbot.java
@@ -1,6 +1,7 @@
 package chatbot;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -8,6 +9,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Vector;
 
 import opennlp.tools.cmdline.parser.ParserTool;
 import opennlp.tools.dictionary.Dictionary;
@@ -95,193 +97,237 @@ public class chatbot {
 		stdin = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Done");
 
-		
-		while (true) {
-			// Parse a sentence
-			String sentences[] = sdetector.sentDetect(readInput());
-			for (String sent : sentences) {
-				Parse[] topParses = ParserTool.parseLine(sent, parser, 3);
-				
-
-				for (Parse p : topParses) {
-					
-					//p.show();
-				}
-				
-				//respond
-				System.out.println(response(topParses));
-			}
-		}
-		
-		/*
+		InputStream is = chatbot.class.getResourceAsStream("/play");
 		NGram ng = new chatbot().new NGram();
-		
-		for(int i =0; i < 4; i++ ) {
-		  ng.parseSentence(readInput());
-		  ng.parseSentence(readInput());
-		  ng.parseSentence(readInput());
-		
-		  System.out.println(ng);
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(is));
+			String sentence;
+			while ((sentence = in.readLine()) != null) {
+				sentence = sentence.replaceAll("[.]", "");
+				sentence = sentence.replaceAll("[?]", "");
+				sentence = sentence.replaceAll("[!]", "");
+				ng.parseSentence(sentence);
+				System.out.println(ng);
+			}
+			System.out.println(ng.dict.toString("ADJP"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.exit(1);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.exit(1);
 		}
-		*/
-		
-		
+
+		/*
+		 * while (true) { // Parse a sentence String sentences[] =
+		 * sdetector.sentDetect(readInput()); for (String sent : sentences) {
+		 * Parse[] topParses = ParserTool.parseLine(sent, parser, 3);
+		 * 
+		 * 
+		 * for (Parse p : topParses) {
+		 * 
+		 * //p.show(); }
+		 * 
+		 * //respond System.out.println(response(topParses)); } }
+		 * 
+		 * 
+		 * NGram ng = new chatbot().new NGram();
+		 * 
+		 * for (int i = 0; i < 4; i++) { ng.parseSentence(readInput());
+		 * ng.parseSentence(readInput()); ng.parseSentence(readInput());
+		 * 
+		 * System.out.println(ng); }
+		 */
 	}
-	
+
 	static String response(Parse[] parsedSentences) {
-		for(Parse child : parsedSentences[0].getChildren()) {
+		for (Parse child : parsedSentences[0].getChildren()) {
 			child.show();
 			System.out.println(child.getType());
-			if(child.getType().equals("SBARQ")) {
+			if (child.getType().equals("SBARQ")) {
 				return "Why do you care?";
 			}
-			if(child.getType().equals("S")) {
-				Parse p = findFirstTag(child,  new String[] {"SBAR"} ); 
-				if(p != null) {
-				  return "You truly believe that " + p.toString() + "?";
+			if (child.getType().equals("S")) {
+				Parse p = findFirstTag(child, new String[] { "SBAR" });
+				if (p != null) {
+					return "You truly believe that " + p.toString() + "?";
 				}
-				
-				p = findFirstTag(child,  new String[] {"NN", "NNS", "SBAR"} ); 
-				if(p != null) {
-					//NN or NNS
-					return "Tell me more about the " +  p.toString();
+
+				p = findFirstTag(child, new String[] { "NN", "NNS", "SBAR" });
+				if (p != null) {
+					// NN or NNS
+					return "Tell me more about the " + p.toString();
 				}
-				
+
 			}
-			if(child.getType().equals("SQ")) {
+			if (child.getType().equals("SQ")) {
 				return "The answer is yes. I like yes or no questions.";
 			}
 		}
 		return "";
 	}
-	
-	
-	//depth first search on the parse tree that returns the first instance of 
-	//parse that is of type matching one of the strings in names
+
+	// depth first search on the parse tree that returns the first instance of
+	// parse that is of type matching one of the strings in names
 	static Parse findFirstTag(Parse tree, String[] names) {
-		for(String s : names) {
-		  if(tree.getType().equals(s)) {
-  			return tree;
-	       }
+		for (String s : names) {
+			if (tree.getType().equals(s)) {
+				return tree;
+			}
 		}
-		
-        for(Parse child : tree.getChildren()) {
-		  Parse p = findFirstTag(child, names);
-		   if(p != null) {
-		     return p;
-		   }
+
+		for (Parse child : tree.getChildren()) {
+			Parse p = findFirstTag(child, names);
+			if (p != null) {
+				return p;
+			}
 		}
-		 
+
 		return null;
 	}
-	
-	
-	
-	public class NGram extends HashMap< String, HashMap<String,Integer> > {
-		
+
+	public class NGram extends HashMap<String, HashMap<String, Integer>> {
+
+		private class dictionary extends HashMap<String, Vector<String>> {
+			public String toString() {
+				String string = null;
+				for (String s : this.keySet()) {
+					string += "Type: " + s + " Examples: \n";
+					for (String v : this.get(s)) {
+						string +="\t" + v + "\n";
+						string += "\n";
+					}		
+					string += "\n\n";
+				}
+
+				return string;
+			}
+
+			public String toString(final String phrase) {
+				String string = "";
+				string += "Type: " + phrase + " Examples: \n";
+				for (String v : this.get(phrase)) {
+					string += "\t" + v + "\n";
+				}
+				string += "\n\n";
+
+				return string;
+			}
+		}
+
+		public dictionary dict = new dictionary();
+
 		public void parseSentence(String sent) {
-			//Parse the sentence
+			// Parse the sentence
 			Parse[] topParse = ParserTool.parseLine(sent, parser, 3);
-			
-			//Crawl the tree
+
+			// Crawl the tree
 			crawl(topParse[0]);
 		}
-		
+
 		public void crawl(Parse p) {
 			String topTag = p.getType();
 			String childTags = "";
-			HashMap<String,Integer> ent = null;
+			HashMap<String, Integer> ent = null;
+			Vector<String> sents = null;
 			Parse[] children = null;
-			
-			if(p.isFlat()) {
+
+			if (p.isFlat()) {
 				return;
 			}
 
-			//Get the children of this node
+			// Get the children of this node
 			children = p.getChildren();
-			if(children == null || children.length == 0) {
+			if (children == null || children.length == 0) {
 				return;
 			}
-			
-			
-			//Check for existence of entry for this parse's N-Gram
-			//If it doesn't exist, make it.
+
+			// Check for existence of entry for this parse's N-Gram
+			// If it doesn't exist, make it.
 			ent = get(topTag);
-			if(ent == null) {
-				put(topTag, new HashMap<String,Integer>());
+			sents = dict.get(topTag);
+			if (ent == null) {
+				put(topTag, new HashMap<String, Integer>());
+				dict.put(topTag, new Vector<String>());
 				ent = get(topTag);
+				sents = dict.get(topTag);
 			}
 
-			//Construct the entry;
-			for(Parse child : children) {
-				  childTags += child.getType() + ",";
+			// Construct the entry;
+			for (Parse child : children) {
+				childTags += child.getType() + ",";
 			}
-			
-		
-			
-			//Increment any existing entry for this leaf NGram
-			Integer val =  ent.get(childTags);
-			if(val == null) { 
+
+			// Increment any existing entry for this leaf NGram
+			Integer val = ent.get(childTags);
+			if (val == null) {
 				val = 0;
 			}
-			
-			//Put the NGram in
-			System.out.println("entry for " + childTags);
-			System.out.println(childTags.length());
+
+			// Put the NGram in
+			//System.out.println("entry for " + childTags);
+			//System.out.println(childTags.length());
 			ent.put(childTags, ++val);
-			
-			//Crawl children
-			for(Parse child : children) {
+
+			String subsentence = "";
+			for (Parse child : children) {
+				subsentence += child + " ";
+			}
+			sents.add(subsentence);
+
+			// Crawl children
+			for (Parse child : children) {
 				crawl(child);
 			}
-			
-			
+
 		}
-		
+
 		public String toString() {
 			String ret = "";
-			HashMap<String,Integer> ent = null;
-			
-			//Iterate over top tags
-			for(String tag : keySet()) {
+			HashMap<String, Integer> ent = null;
+
+			// Iterate over top tags
+			for (String tag : keySet()) {
 				ret += "NGRAM for : " + tag + "\n";
 				ret += NGramEnt2Str(get(tag)) + "\n\n";
 			}
-			
-			
+
 			return ret;
 		}
 
 		class Pair implements Comparator<Pair> {
 			public final Integer val;
 			public final String name;
+
 			public Pair(String name, int val) {
 				this.name = name;
 				this.val = val;
 			}
-			//Sort high to high
+
+			// Sort high to high
 			@Override
 			public int compare(Pair o1, Pair o2) {
 				return o2.val.compareTo(o1.val);
 			}
 		}
-		
-		public String NGramEnt2Str(HashMap<String,Integer> ent) {
-            String ret = "";
+
+		public String NGramEnt2Str(HashMap<String, Integer> ent) {
+			String ret = "";
 			LinkedList<Pair> list = new LinkedList<Pair>();
-			for(String name : ent.keySet()) {
+			for (String name : ent.keySet()) {
 				list.add(new Pair(name, ent.get(name)));
 			}
 
-			//Sort it with descending values
-			Collections.sort(list, list.get(0) );
-			
-			for(Pair p: list) {
+			// Sort it with descending values
+			Collections.sort(list, list.get(0));
+
+			for (Pair p : list) {
 				ret += p.name + " : " + p.val + "\n";
 			}
-			
+
 			return ret;
 		}
 	}
 }
-
