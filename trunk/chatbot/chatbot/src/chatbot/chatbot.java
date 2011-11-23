ad09de2b4c9b44b1f05fd7f3fa2bef149182f481
kennylmay <kennylmay@bc7d2a11-9811-4662-8751-9abf3bd84159>
2011-11-23 15:42:46 +0000

    Getting rid of unused code

--- trunk/chatbot/chatbot/src/chatbot/chatbot.java
+++ trunk/chatbot/chatbot/src/chatbot/chatbot.java
@@ -1,20 +1,15 @@
 package chatbot;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
-import java.util.Iterator;
-
 import net.didion.jwnl.JWNL;
 import net.didion.jwnl.JWNLException;
 import net.didion.jwnl.data.IndexWord;
 import net.didion.jwnl.data.POS;
-import net.didion.jwnl.data.PointerType;
 import net.didion.jwnl.data.Synset;
-import net.didion.jwnl.data.relationship.RelationshipFinder;
-import net.didion.jwnl.data.relationship.RelationshipList;
-import net.didion.jwnl.data.relationship.SymmetricRelationship;
 import net.didion.jwnl.dictionary.Dictionary;
-import chatbot.TestDefaults;
 import opennlp.tools.cmdline.parser.ParserTool;
 import opennlp.tools.parser.Parse;
 
@@ -22,6 +17,15 @@ public class chatbot {
 
 	public static void main(String[] args) {
 		System.out.println("Initializing...");
+		try {
+			JWNL.initialize(new FileInputStream("./res/file_properties.xml"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JWNLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		Global.loadModels();
 		// open up standard input
 		Global.stdin = new BufferedReader(new InputStreamReader(System.in));
@@ -59,14 +63,13 @@ public class chatbot {
 	
 	public static void testMorphological() {
 		try {  
-			JWNL.initialize(TestDefaults.getInputStream());
+			
 			IndexWord iw = Dictionary.getInstance().lookupIndexWord(POS.VERB, "running-away");
 			System.out.println("Index word : " + iw.toString());
 			System.out.println("Senses for the word");
 			for (Synset sense : iw.getSenses()){
           		System.out.println(sense.toString());
-          	}
-			
+          	}	
 			
 		} catch (JWNLException e) {
 			e.printStackTrace();
