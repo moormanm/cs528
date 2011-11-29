0c31c648debe3f5524f8dcaeae760ca8dc757341
kennylmay@gmail.com <kennylmay@gmail.com@bc7d2a11-9811-4662-8751-9abf3bd84159>
2011-11-29 02:09:37 +0000

    Fixing find isHypernmOf

--- trunk/chatbot/chatbot/src/chatbot/WordRelations.java
+++ trunk/chatbot/chatbot/src/chatbot/WordRelations.java
@@ -1,35 +1,48 @@
 package chatbot;
 
+import java.util.Iterator;
+
 import net.didion.jwnl.JWNLException;
 import net.didion.jwnl.data.IndexWord;
 import net.didion.jwnl.data.POS;
-import net.didion.jwnl.data.Pointer;
 import net.didion.jwnl.data.PointerType;
-import net.didion.jwnl.data.Synset;
+import net.didion.jwnl.data.relationship.RelationshipFinder;
+import net.didion.jwnl.data.relationship.RelationshipList;
 import net.didion.jwnl.dictionary.Dictionary;
+import net.didion.jwnl.data.relationship.Relationship;
+import net.didion.jwnl.data.relationship.AsymmetricRelationship;
 
 public class WordRelations {
 
-	public boolean isHypernymOf(String input, POS posType, String targetWord) {
+	public static boolean isHypernymOf(String input, POS posType, String target) {
 
-		IndexWord indexWord;
+
 		try {
-			indexWord = Dictionary.getInstance().getIndexWord(posType, input);
-			if (indexWord == null) {
+			IndexWord inputWord;
+			inputWord = Dictionary.getInstance().getIndexWord(posType, input);
+			if (inputWord == null) {
 				return false;
 			}
+			
+			IndexWord targetWord;
+			targetWord = Dictionary.getInstance().getIndexWord(posType, target);
+			
+			if (targetWord == null) {
+				return false;
+			}
+			
+			RelationshipList list = RelationshipFinder.getInstance().findRelationships(inputWord.getSense(1), targetWord.getSense(1), PointerType.HYPERNYM);
+			
 
-			Synset[] set = indexWord.getSenses();
-			Pointer[] pointer = set[0].getPointers(PointerType.HYPERNYM);
-
-			for (Pointer x : pointer) {
-				Synset target = x.getTargetSynset();
-				if (target.getWord(0).getLemma().equals(targetWord)) {
-					return true;
-				}
+			for (Iterator itr = list.iterator(); itr.hasNext();) {
+				((Relationship) itr.next()).getNodeList().print();
 			}
 
-			return false;
+			if (((AsymmetricRelationship) list.get(0)).getCommonParentIndex() < 4){
+				return true;
+			}else{
+				return false;
+			}
 
 		} catch (JWNLException e) {
 			// TODO Auto-generated catch block
